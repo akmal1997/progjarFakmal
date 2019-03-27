52737765468c894f5ea767640759fb119cfc403a
Akmal Darari <akmalflightsimmer@gmail.com>
2019-03-27 07:36:46 +0700

    Upload

---
+++ Tugas 2/client4.py
@@ -0,0 +1,39 @@
+import socket
+import select
+import os
+
+udp_ip = "127.0.0.4"
+port = 9000
+timeout = 3
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((udp_ip, port))
+
+path = './client4/'
+if not os.path.exists(path):
+	os.makedirs(path)
+
+while True:
+	data, addr = sock.recvfrom(1024)
+	if data:
+		print "File name:", data
+		file_name = data.strip()
+		print file_name
+		name = os.path.splitext(file_name)[0]
+		file_extension = os.path.splitext(file_name)[1]
+		print file_extension
+	filenamedest = name + 'copy4' + file_extension
+	print filenamedest
+	complete = os.path.join(path, filenamedest)
+	f = open(complete, 'wb')
+	
+	while True:
+		ready = select.select([sock], [], [], timeout)
+		if ready[0]:
+			data, addr = sock.recvfrom(1024)
+			f.write(data)
+		else:
+			print "%s Finish!" % file_name
+			f.close()
+			#sock.close()
+			break
+	
---
+++ Tugas 2/server.py
@@ -0,0 +1,34 @@
+import socket
+import time
+import sys
+import os
+
+ip = ["127.0.0.1", "127.0.0.2", "127.0.0.3", "127.0.0.4"]
+port = 9000
+buf = 1024
+file_name=["puong.jpg", "bader.PNG", "anggrek.jpg"]
+
+def send_file(filename, address, ports):
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	#print f
+	f = filename
+	size = os.stat(f).st_size
+	sock.sendto(f, (address, ports))
+	print "Sending %s ..." %f
+	sent = 0
+	f = open(f, "rb")
+	data = f.read(buf)
+	while (data):
+		if(sock.sendto(data, (address, ports))):
+			data = f.read(buf)
+			sent+=1
+			print 'Sent {} from {}'. format(sent, size)
+	sock.close()
+	f.close()
+	time.sleep(10)
+
+
+for ip_address in ip:
+	for i in range(len(file_name)):
+		send_file(file_name[i], ip_address, port)
